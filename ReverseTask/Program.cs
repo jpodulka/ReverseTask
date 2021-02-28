52fe5c2a642d6d0a189e902524654ca7656636fd
Jedrzej Podulka <jpodulka@gmail.com>
2021-02-28 20:59:49 +0100

    Dodaj pliki projektów.

---
+++ ReverseTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "the sky is blue";
+            Console.WriteLine(sentence);
+            
+            var reversedtext = Utils.Reverse(sentence);
+
+            Console.WriteLine(reversedtext);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReverseTask/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseTask
+{
+    public static class Utils
+    {
+        public static string Reverse(string text)
+        {
+            var s = text.Split(' ');
+
+            string ans = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                ans += s[i] + " ";
+            }
+            return ans;
+        }
+    }
+}
